6469d00977a9884147052fde145da5750cd849f4
NatuMyers <me@natumyers.com>
2017-01-29 17:57:59 -0500

    payload and link use basic navigation attempts

--- static/js/custom.js
+++ static/js/custom.js
@@ -17,3 +17,15 @@ $('#knack').hover(function(){
 }, function(){
     $(this).removeClass('animated');
 });
+
+
+function goUp() {
+  var directory = payload.split('/'); // http://stackoverflow.com/questions/5269856/how-to-split-comma-separated-string-using-javascript
+  // Basically cut off the last part of the array to go up a directory.
+
+  if (directory.length >= 1) { // else, it's the root directory anyway.
+    var payload = payload.replace(directory[directory.length - 1], '');
+  }
+
+    window.location.href = "payload="+payload; // http://stackoverflow.com/questions/1226714/how-to-get-browser-to-navigate-to-url-in-javascript
+}
